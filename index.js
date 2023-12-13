2ad47bea827b90e75b733ff58628a09724743004
krasimir.georgiev <krasimir.georgiev@novarto.com>
2023-12-13 11:48:24 +0200

    test

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const User = require('./models/User');
 const Role = require('./models/Role');
 // const authRout = require('./routes/auth')
 
-const CLIENT_URL = "https://deluxe-tapioca-7f56a1.netlify.app/";//"http://localhost:3000/";
+const CLIENT_URL = "https://deluxe-tapioca-7f56a1.netlify.app";//"http://localhost:3000/";
 const cookieSession = require('cookie-session');
 const session = require('express-session');
 const express = require('express');
