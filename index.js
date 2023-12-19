0196360422e76d11ed49a61849787c630096aa66
krasimir.georgiev <krasimir.georgiev@novarto.com>
2023-12-19 16:16:20 +0200

    cors

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(passport.session());
 
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: process.env.CLIENT_URL,
 		methods: "GET,POST,PUT,DELETE",
 		credentials: true,
 	})
