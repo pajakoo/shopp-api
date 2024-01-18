8675893ef23b3556111b6acaeb42e51834da0954
krasimir.georgiev <krasimir.georgiev@novarto.com>
2024-01-18 08:09:42 +0200

    perfeeettto

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(
 passport.use(new GoogleStrategy({
   clientID:  process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3333/auth/google/callback',
+  callbackURL: `${process.env.CLIENT_URL}/auth/google/callback`,
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
